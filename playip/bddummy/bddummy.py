ea1bd6de84a49ddae7d55d12bdf192ff3bf45dce
jesjf <jorge.filho@jambeiro.com.br>
2021-12-28 16:41:52 -0300

    Coloquei usuários alternativos para testar acessos diferentes ao smartOLT e obter resultados diferentes.

--- playip/bddummy/bddummy.py
+++ playip/bddummy/bddummy.py
@@ -18,7 +18,7 @@ class ContractData(pydantic.BaseModel):
     download_speed: int = 0
     upload_speed:int = 0
     is_radio:bool = False
-    is_ftth: bool
+    is_ftth: bool = False
     found:bool = False
     pack_name:str = "PackDefault"
     home_access_key:Optional[str] = None
@@ -27,9 +27,14 @@ class ContractData(pydantic.BaseModel):
 
 @bdbasicrouter.get("/getcontract/{id_contract}", response_model=ContractData)
 async def getContract(id_contract:str) -> ContractData:
+
+    #hak = "william1.am.ftth"
+    #hak = "ivone2.sr.ftth"
+    hak =  "gilvanete.sb.ftth"
+
     if id_contract == "13000":
         return ContractData(id_contract=id_contract,download_speed=100,upload_speed=50, is_radio=False, is_ftth=True,
-                            home_access_key="ivone2.sr.ftth", home_access_type="smartolt",
+                            home_access_key=hak, home_access_type="smartolt",
                             found=True, )
     else:
         return ContractData(id_contract=id_contract, found=False)
