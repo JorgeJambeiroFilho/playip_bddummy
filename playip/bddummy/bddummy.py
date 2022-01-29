4b5c3b453a850a968ac9e9ca4707660f09fc6f50
jesjf <jorge.filho@jambeiro.com.br>
2022-01-28 21:04:50 -0300

    Ajustei valores default no pydantic model de contrato.

--- playip/bddummy/bddummy.py
+++ playip/bddummy/bddummy.py
@@ -12,17 +12,18 @@ print("FASTAPI version ",fastapi.__version__)
 
 bdbasicrouter = APIRouter(prefix="/playipispbd/basic")
 
-
 class ContractData(pydantic.BaseModel):
-    id_contract: str = 0
-    download_speed: int = 0
-    upload_speed:int = 0
-    is_radio:bool = False
-    is_ftth: bool = False
-    found:bool = False
-    pack_name:str = "PackDefault"
-    home_access_key:Optional[str] = None
-    home_access_type:Optional[str] = None
+    id_contract: str
+    found: bool = False
+    download_speed: Optional[int] = None
+    upload_speed: Optional[int] = None
+    is_radio: Optional[bool] = None
+    is_ftth: Optional[bool] = None
+    pack_name: Optional[str] = None
+    user_name: Optional[str] = None
+    home_access_key: Optional[str] = None
+    home_access_type: Optional[str] = None
+
 
 
 @bdbasicrouter.get("/getcontract/{id_contract}", response_model=ContractData)
@@ -33,9 +34,9 @@ async def getContract(id_contract:str) -> ContractData:
     #hak =  "gilvanete.sb.ftth"
 
     if id_contract == "13000":
-        return ContractData(id_contract=id_contract,download_speed=100,upload_speed=50, is_radio=False, is_ftth=True,
+        return ContractData(found=True, id_contract=id_contract,download_speed=100,upload_speed=50, is_radio=False, is_ftth=True,
                             home_access_key=hak, home_access_type="smartolt",
-                            found=True, )
+                            pack_name="PackDefault")
     else:
         return ContractData(id_contract=id_contract, found=False)
 
