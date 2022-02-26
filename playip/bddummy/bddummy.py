bc4d3c79635737c8eb32fc7ae47046466ecd4ff2
jesjf <jorge.filho@jambeiro.com.br>
2022-02-26 13:34:52 -0300

    Incluí o endereço nos dados do contrato

--- playip/bddummy/bddummy.py
+++ playip/bddummy/bddummy.py
@@ -12,6 +12,15 @@ print("FASTAPI version ",fastapi.__version__)
 
 bdbasicrouter = APIRouter(prefix="/playipispbd/basic")
 
+class Endereco(pydantic.BaseModel):
+    logradouro: str
+    numero: str
+    complemento: str
+    cep: str
+    condominio: Optional[str]
+    cidade: str
+
+
 class ContractData(pydantic.BaseModel):
     id_contract: str
     found: bool = False
@@ -23,7 +32,7 @@ class ContractData(pydantic.BaseModel):
     user_name: Optional[str] = None
     home_access_key: Optional[str] = None
     home_access_type: Optional[str] = None
-
+    endereco: Optional[Endereco] = None
 
 
 @bdbasicrouter.get("/getcontract/{id_contract}", response_model=ContractData)
@@ -34,9 +43,10 @@ async def getContract(id_contract:str) -> ContractData:
     #hak =  "gilvanete.sb.ftth"
 
     if id_contract == "13000":
+        endereco: Endereco = Endereco(logradouro="Rua Etiopia", numero="33", complemento="", cep="06660070", cidade="Itapevi")
         return ContractData(found=True, id_contract=id_contract,download_speed=100,upload_speed=50, is_radio=False, is_ftth=True,
                             home_access_key=hak, home_access_type="smartolt",
-                            pack_name="PackDefault")
+                            pack_name="PackDefault", endereco=endereco)
     else:
         return ContractData(id_contract=id_contract, found=False)
 
