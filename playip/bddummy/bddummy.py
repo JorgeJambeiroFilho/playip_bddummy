ee66f6012701adeca6a98913e6026b6420d16811
jesjf <jorge.filho@jambeiro.com.br>
2022-02-27 15:37:12 -0300

    Implementei a API da URA.

--- playip/bddummy/bddummy.py
+++ playip/bddummy/bddummy.py
@@ -34,6 +34,38 @@ class ContractData(pydantic.BaseModel):
     home_access_type: Optional[str] = None
     endereco: Optional[Endereco] = None
 
+class Client(pydantic.BaseModel):
+    found: bool = False
+    id_client: Optional[str] = None
+    name: Optional[str] = None
+    alt_name: Optional[str] = None
+    cpfcnpj: Optional[str] = None
+
+@bdbasicrouter.get("/getclientfromcpfcnpj/{cpfcnpj}", response_model=Client)
+async def getClientFromCPFCNPJ(cpfcnpj:str) -> Client:
+    if cpfcnpj=="07983764499":
+        return Client(found=True, id_client="15594", name="Ivone", cpfcnpj=cpfcnpj)
+
+@bdbasicrouter.get("/getcontracts/{id_client}", response_model=List[ContractData])
+async def getContracts(id_client: str) -> List[ContractData]:
+    contract_list: List[str] = []
+    if id_client == "15594":
+        contract_list.append("13000")
+    contracts: List[ContractData] = []
+    for c in contract_list:
+        contract: ContractData = await getContract(c)
+        contracts.append(contract)
+    return contracts
+
+@bdbasicrouter.get("/getcontractsfromcpfcnpj/{cpfcnpj}", response_model=List[ContractData])
+async def getContractsFromCPFCNPJ(cpfcnpj: str) -> List[ContractData]:
+    client: Client = await getClientFromCPFCNPJ(cpfcnpj)
+    if client:
+        contracts: List[ContractData] = await getContracts(client.id_client)
+        return contracts
+    else:
+        return []
+
 
 @bdbasicrouter.get("/getcontract/{id_contract}", response_model=ContractData)
 async def getContract(id_contract:str) -> ContractData:
