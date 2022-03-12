3efa069d9df1c6618b768e91c372e575456fb8f4
jesjf <jorge.filho@jambeiro.com.br>
2022-03-12 10:09:56 -0300

    Passei a desviar clientes bloqueados antes de buscar problemas técnicos.

--- playip/bddummy/bddummy.py
+++ playip/bddummy/bddummy.py
@@ -54,7 +54,7 @@ async def getContract(id_contract:str) -> ContractData:
         endereco: Endereco = Endereco(logradouro="Rua Etiopia", numero="33", complemento="", bairro="Jardim Santa Rita", cep="06660070", cidade="Itapevi", uf="SP")
         return ContractData(found=True, id_contract=id_contract,download_speed=100,upload_speed=50, is_radio=False, is_ftth=True,
                             home_access_key=hak, home_access_type="smartolt",
-                            pack_name="PackDefault", endereco=endereco)
+                            pack_name="PackDefault", endereco=endereco, bloqueado=False)
     else:
         return ContractData(id_contract=id_contract, found=False)
 
