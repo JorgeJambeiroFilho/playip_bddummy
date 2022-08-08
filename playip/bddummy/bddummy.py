4a7892140cdefcf2a67b33d6a6b36686ed4c3935
jesjf <jorge.filho@jambeiro.com.br>
2022-08-07 22:06:32 -0300

    Incluí datas de ativação e cancelamento nos contratos retornados.

--- playip/bddummy/bddummy.py
+++ playip/bddummy/bddummy.py
@@ -1,3 +1,5 @@
+import datetime
+from datetime import date
 from typing import Optional
 from typing import List
 from dynaconf import settings
@@ -12,6 +14,7 @@ from playipappcommons.infra.endereco import Endereco
 from playipappcommons.ispbd.ispbddata import Client, ContractData
 
 print("FASTAPI version ",fastapi.__version__)
+print("Prints")
 
 bdbasicrouter = APIRouter(prefix="/playipispbd/basic")
 
@@ -20,6 +23,7 @@ bdbasicrouter = APIRouter(prefix="/playipispbd/basic")
 
 @bdbasicrouter.get("/getclientfromcpfcnpj/{cpfcnpj}", response_model=Client)
 async def getClientFromCPFCNPJ(cpfcnpj:str, auth=Depends(defaultpermissiondep)) -> Client:
+    print("get client ", cpfcnpj)
     if cpfcnpj=="07983764499":
         return Client(found=True, id_client="15594", name="Ivone", cpfcnpj=cpfcnpj)
     if cpfcnpj=="29875914894":
@@ -27,6 +31,7 @@ async def getClientFromCPFCNPJ(cpfcnpj:str, auth=Depends(defaultpermissiondep)) 
 
 @bdbasicrouter.get("/getcontracts/{id_client}", response_model=List[ContractData])
 async def getContracts(id_client: str, auth=Depends(defaultpermissiondep)) -> List[ContractData]:
+    print("get contracts client ",id_client)
     contract_list: List[str] = []
     if id_client == "15594":
         contract_list.append("13000")
@@ -41,6 +46,7 @@ async def getContracts(id_client: str, auth=Depends(defaultpermissiondep)) -> Li
 
 @bdbasicrouter.get("/getcontractsfromcpfcnpj/{cpfcnpj}", response_model=List[ContractData])
 async def getContractsFromCPFCNPJ(cpfcnpj: str, auth=Depends(defaultpermissiondep)) -> List[ContractData]:
+    print("get contracts cpfcnpj", cpfcnpj)
     client: Client = await getClientFromCPFCNPJ(cpfcnpj)
     if client:
         contracts: List[ContractData] = await getContracts(client.id_client)
@@ -52,20 +58,25 @@ async def getContractsFromCPFCNPJ(cpfcnpj: str, auth=Depends(defaultpermissionde
 @bdbasicrouter.get("/getcontract/{id_contract}", response_model=ContractData)
 async def getContract(id_contract:str, auth=Depends(defaultpermissiondep)) -> ContractData:
 
+    print("get contract ", id_contract)
     #hak = "william1.am.ftth"
     hak = "ivone2.sr.ftth"
     #hak =  "gilvanete.sb.ftth"
     if id_contract == "19700":
+        print(19700)
         endereco: Endereco = Endereco(logradouro="Praça Alpha de Centauro (Centro de Apoio 2)", numero="20", complemento="sala7", bairro="Alphaville", cep="06541075", cidade="Santana de Parnaíba", uf="SP", prefix=None)
         return ContractData(found=True, id_contract=id_contract,download_speed=100,upload_speed=50, is_radio=False, is_ftth=True,
                             home_access_key=hak, home_access_type="smartolt",
-                            pack_name="PackDefault", endereco=endereco, bloqueado=False)
+                            pack_name="PackDefault", endereco=endereco, bloqueado=False,
+                            dt_ativacao=datetime.datetime(year=2018, month=1, day=1).timestamp(), dt_cancelamento=None)
 
     elif id_contract == "13000":
+        print(13000)
         endereco: Endereco = Endereco(logradouro="Rua Etiopia", numero="33", complemento="", bairro="Jardim Santa Rita", cep="06660070", cidade="Itapevi", uf="SP", prefix=None)
         return ContractData(found=True, id_contract=id_contract,download_speed=100,upload_speed=50, is_radio=False, is_ftth=True,
                             home_access_key=hak, home_access_type="smartolt",
-                            pack_name="PackDefault", endereco=endereco, bloqueado=False)
+                            pack_name="PackDefault", endereco=endereco, bloqueado=False,
+                            dt_ativacao=datetime.datetime(year=2018, month=1, day=1).timestamp(), dt_cancelamento=None)
     else:
         return ContractData(id_contract=id_contract, found=False)
 
